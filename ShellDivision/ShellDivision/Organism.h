80170060cdc653fbb346225a876b93ac9c4f0243
bluemelodia <hsumelanie@yahoo.com>
2016-02-24 22:49:26 -0500

    Created an organism.

---
+++ ShellDivision/ShellDivision/Organism.h
@@ -0,0 +1,13 @@
+//
+//  Organism.h
+//  ShellDivision
+//
+//  Created by Melanie Lislie Hsu on 2/24/16.
+//  Copyright © 2016 Melanie Lislie Hsu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Organism : NSObject
+
+@end
---
+++ ShellDivision/ShellDivision/Organism.m
@@ -0,0 +1,13 @@
+//
+//  Organism.m
+//  ShellDivision
+//
+//  Created by Melanie Lislie Hsu on 2/24/16.
+//  Copyright © 2016 Melanie Lislie Hsu. All rights reserved.
+//
+
+#import "Organism.h"
+
+@implementation Organism
+
+@end
