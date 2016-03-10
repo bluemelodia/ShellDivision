e18a00d3d57ce77e0e784abe608034c2bd0fb2ba
bluemelodia <hsumelanie@yahoo.com>
2016-03-10 11:25:06 -0500

    Built simple gridView, need to configure the cells.

--- ShellDivAndroid/app/src/main/java/com/example/bluemelodia/shelldivision/ImageAdapter.java
+++ ShellDivAndroid/app/src/main/java/com/example/bluemelodia/shelldivision/ImageAdapter.java
@@ -1,7 +1,54 @@
 package com.example.bluemelodia.shelldivision;
 
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.content.Context;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.view.View;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by bluemelodia on 3/10/16.
  */
-public class ImageAdapter {
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return 64;
+    }
+
+    // return the actual object at the specified position in the adapter
+    public Object getItem(int position) {
+        return null;
+    }
+
+    // return the row id of the item
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(260, 260));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setAlpha(0.2f);
+            imageView.setPadding(0, 0, 0, 0);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+        return imageView;
+    }
 }
--- ShellDivAndroid/app/src/main/java/com/example/bluemelodia/shelldivision/MainActivity.java
+++ ShellDivAndroid/app/src/main/java/com/example/bluemelodia/shelldivision/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.bluemelodia.shelldivision;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static ImageAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final GridView gridView = (GridView) findViewById(R.id.gridView);
+        // set a custom adapter (ImageAdapter) as the source for all items to be displayed in the grid
+        adapter = new ImageAdapter(this);
+        gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
     }
 }
